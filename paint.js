c0b11a7b6b1fcc6f9d23cb79c195682bfab15d51
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-02 11:04:38 +0300

    js/Dynamically click tools buttons and edit select the active class

--- paint.js
+++ paint.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("canvas")
+toolBtns = document.querySelectorAll(".tool")
 
 ctx = canvas.getContext("2d") // getContext() method returns a drawing context on the canvas
 
@@ -25,6 +26,15 @@ const drawing = (e) => {
     ctx.lineTo(e.offsetX, e.offsetY) // creating line according to mouse pointer
     ctx.stroke() //drawing/filling line with color
 }
+
+toolBtns.forEach(btn => {
+    btn.addEventListener("click", () => { // adding click event to all tool option
+        // changing dynamically the active class
+        document.querySelector(".options .active").classList.remove("active")
+        btn.classList.add("active")
+        console.log(btn.id)
+    })    
+});
 
 canvas.addEventListener("mousedown", startDraw)
 canvas.addEventListener("mousemove", drawing)
